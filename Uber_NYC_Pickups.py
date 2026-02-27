b1b6f6dcd254b8afe168b85fc227bb6e8615ee42
shilphan <sgphanse@gmail.com>
2026-02-28 02:29:27 +0530

    Update Uber_NYC_Pickups.py

--- Uber_NYC_Pickups.py
+++ Uber_NYC_Pickups.py
@@ -29,12 +29,11 @@ if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
-st.subheader('Number of pickups by hour')
-
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-
-st.bar_chart(hist_values)
+if st.checkbox('Show bar chart'):
+    st.subheader('Number of pickups by hour')
+    hist_values = np.histogram(
+        data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+    st.bar_chart(hist_values)
 
 st.subheader('Map of all pickups')
 st.map(data)
